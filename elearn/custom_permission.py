fd686be317f6f8e143d45c7554f75d8dd3afb135
yugan747 <iamyugdashaudi@gmail.com>
2022-10-20 15:05:59 +0545

    indient error fix

--- elearn/custom_permission.py
+++ elearn/custom_permission.py
@@ -3,9 +3,15 @@ from rest_framework.permissions import BasePermission
 class AdminCheckPermission(BasePermission):
     def has_permission(self, request, view):
         if not request.user.is_superuser:
+    
             return False
+       
 
 class DeveloperAdminCheckPermission(BasePermission):
     def has_permission(self, request, view):
-       if not (request.user.is_superuser and request.user.user_role == "developer"):
-        return False
+        if not (request.user.is_superuser and request.user.user_role == "developer"):
+            return False
+
+       
+
+        
